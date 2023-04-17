f3ade2d8d3ca7efc74d2c26c9df09e3c67aa9b5a
Raje Abhilash Mohite (Chavhan Hambirrao) <rajeabhilashmohite@outlook.com>
2023-04-17 13:18:01 +0530

    Dashboard UI Design Started

--- lib/mobile/dashboard/dashboard.dart
+++ lib/mobile/dashboard/dashboard.dart
@@ -36,52 +36,123 @@ class _DashBoardState extends State<DashBoard> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-              children: [
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 200,
-                        height: 200,
-                        margin: EdgeInsets.only(left: 200),
-                        decoration: BoxDecoration(
-                            gradient: const LinearGradient(
-                              colors: [
-                                Colors.teal,
-                                Colors.tealAccent,
-                              ],
-                              begin: Alignment.topRight,
-                              end: Alignment.bottomLeft,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              addSpacer(8.0),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Good Morning,",
+                          style: TextStyle(
+                            fontFamily: 'Exo',
+                            fontSize: 22,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 234,
+                          child: Text(
+                            "Raje Abhilash Mohite".toUpperCase(),
+                            maxLines: 1,
+                            softWrap: false,
+                            overflow: TextOverflow.fade,
+                            style: const TextStyle(
+                              fontFamily: 'Exo',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 26,
                             ),
-                            borderRadius: BorderRadius.circular(100)),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 100,
-                        margin: EdgeInsets.only(right: 270),
-                        decoration: BoxDecoration(
-                            gradient: const LinearGradient(
-                              colors: [
-                                Colors.teal,
-                                Colors.tealAccent,
-                              ],
-                              begin: Alignment.topRight,
-                              end: Alignment.bottomLeft,
-                            ),
-                            borderRadius: BorderRadius.circular(100)),
-                      )
-                    ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Spacer(),
+                    SizedBox(
+                      width: 74,
+                      child: Image.asset('asset/Brand/Black_Wire.png'),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 4, 12, 4),
+                child: Card(
+                  elevation: 2,
+                  child: Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black, width: 1.0)),
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 4, 12, 4),
+                child: Card(
+                  elevation: 2,
+                  child: Container(
+                    height: 250,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black, width: 1.0)),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(12, 4, 12, 4),
+                child: Card(
+                  elevation: 2,
+                  child: Container(
+                    height: 400,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                        color: Colors.white,
+                        border: Border.all(color: Colors.black, width: 1.0)),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        surfaceTintColor: Colors.white,
+        elevation: 5,
+        height: 64,
+        child: SizedBox(
+          height: 60,
+          width: double.maxFinite,
+          // decoration: BoxDecoration(
+          //     color: Colors.white,
+          //     borderRadius: BorderRadius.vertical(top: Radius.circular(20.0))),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              SizedBox(
+                width: 64,
+                child: Image.asset('asset/DarkSutra.png'),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.start),
+                label: Text("Let's Start"),
+              ),
+            ],
+          ),
         ),
       ),
     );
